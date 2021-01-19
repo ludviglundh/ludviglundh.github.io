54477c6ba69c6cea972f4021e0256f2f39c31e37
Ludvig Lundh <ludvig.lundh@ancon.io>
2021-01-19 11:21:11 +0100

    add under construction

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>🚧 Under construction 🚧</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ludviglundh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to my github page
         </a>
       </header>
     </div>
